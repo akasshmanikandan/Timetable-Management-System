f572e64d63fdf88442629323253d0b0b36f574b9
nbv3 <nbv3@student.le.ac.uk>
2025-01-02 22:53:38 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,192 @@
 package timetable;
 
-import java.io.*;
-import java.util.List;
+
 import java.util.Scanner;
+
+import timetable.assignmet.AssignmentService;
+
 
 public class Main {
     public static void main(String[] args) {
-        TimetableService timetableService = new TimetableService();
+        // initializes services with default file 
+        TimetableService timetableService = new TimetableService("timetable_data.ser");
+        AssignmentService assignmentService = new AssignmentService("assignment_data.ser");
+
         Scanner scanner = new Scanner(System.in);
-        int choice=4;
+        int choice = 3;
 
         do {
-            System.out.println("\n=== ||Timetable-Management-System|| ===");
-            System.out.println("1. Add Timetable Event");
-            System.out.println("2. See Timetable Events");
-            System.out.println("3. Delete a Timetable Entry");
-            System.out.println("4. Exit");
+            System.out.println("\n=== || Welcome To Timetable Management System || ===");
+            System.out.println("1. Manage Timetables");
+            System.out.println("2. Manage Assignments");
+            System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
 
-            //handling the menu
             if (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please enter a number between 1 and 4.");
-                scanner.next(); // consume invalid input
-                continue;       // re-directs the menu
+                System.out.println("Invalid input. Please enter correct number.");
+                scanner.next(); // invalid input
+                continue;
             }
 
             choice = scanner.nextInt();
-            scanner.nextLine(); // new line
+            scanner.nextLine(); //newline
 
             switch (choice) {
                 case 1:
-                    // add a new timetable event
+                    manageTimetable(scanner, timetableService);
+                    break;
+
+                case 2:
+                    manageAssignments(scanner, assignmentService);
+                    break;
+
+                case 3:
+                    System.out.println("Exiting System.");
+                    break;
+
+                default:
+                    System.out.println("Please try again.");
+            }
+        } while (choice != 3);
+
+        scanner.close();
+    }
+
+    private static void manageTimetable(Scanner scanner, TimetableService timetableService) {
+        int timetableChoice = 5;
+        do {
+            System.out.println("\n=== || Timetable Management || ===");
+            System.out.println("1. Add Timetable Event");
+            System.out.println("2. View Timetable Events");
+            System.out.println("3. Update Timetable Event");
+            System.out.println("4. Delete Timetable Event");
+            System.out.println("5. Back to Main Menu");
+            System.out.print("Enter your choice: ");
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid! Please enter a number between 1 and 5.");
+                scanner.next(); // invalid input
+                continue;
+            }
+
+            timetableChoice = scanner.nextInt();
+            scanner.nextLine(); //newline
+
+            switch (timetableChoice) {
+                case 1:
                     System.out.print("Enter day: ");
                     String day = scanner.nextLine().trim();
-                    System.out.print("Enter time (00:00 AM format): ");
+                    System.out.print("Enter time (00:00 AM): ");
                     String time = scanner.nextLine().trim();
-                    System.out.print("Enter Subject: ");
+                    System.out.print("Enter subject: ");
                     String subject = scanner.nextLine().trim();
-                    System.out.print("Enter Factulty Name: ");
+                    System.out.print("Enter teacher: ");
                     String teacher = scanner.nextLine().trim();
                     timetableService.addTimetableEntry(day, time, subject, teacher);
                     break;
 
                 case 2:
-                    //shows all events
                     timetableService.displayTimetable();
                     break;
 
                 case 3:
-                    // deletes a timetable entry
-                    System.out.print("Enter day to delete: ");
-                    String deleteDay = scanner.nextLine().trim();
-                    System.out.print("Enter time to delete: ");
-                    String deleteTime = scanner.nextLine().trim();
-                    timetableService.deleteTimetableEntry(deleteDay, deleteTime);
+                    System.out.print("Enter faculty name to update: ");
+                    String oldDay = scanner.nextLine().trim();
+                    System.out.print("Enter time of the event to update: ");
+                    String oldTime = scanner.nextLine().trim();
+                    System.out.print("Enter new day: ");
+                    String newDay = scanner.nextLine().trim();
+                    System.out.print("Enter new time: ");
+                    String newTime = scanner.nextLine().trim();
+                    System.out.print("Enter new subject: ");
+                    String newSubject = scanner.nextLine().trim();
+                    System.out.print("Enter new faculty name: ");
+                    String newTeacher = scanner.nextLine().trim();
+                    timetableService.updateTimetableEntry(oldDay, oldTime, newDay, newTime, newSubject, newTeacher);
                     break;
 
                 case 4:
-                    // exit from program
-                    System.out.println("Exiting Timetable-Management-System.");
+                    System.out.print("Enter faculty name to delete: ");
+                    String deleteTeacher = scanner.nextLine().trim();
+                    System.out.print("Enter time to delete: ");
+                    String deleteTime = scanner.nextLine().trim();
+                    timetableService.deleteTimetableEntry(deleteTeacher, deleteTime);
+                    break;
+
+                case 5:
+                    System.out.println("Returning to Main Menu...");
                     break;
 
                 default:
-                    // invalid menu
-                    System.out.println("Invalid choice!. Please enter a number between 1 and 4.");
+                    System.out.println("Invalid choice. Please try again.");
             }
-        } 
-        while (choice != 4);
-
-        scanner.close();
-    } 
+        } while (timetableChoice != 5);
     }
 
+    private static void manageAssignments(Scanner scanner, AssignmentService assignmentService) {
+        int assignmentChoice = 5;
+        do {
+            System.out.println("\n=== || Assignment Management || ===");
+            System.out.println("1. Add Assignment");
+            System.out.println("2. View Assignments");
+            System.out.println("3. Update Assignment");
+            System.out.println("4. Delete Assignment");
+            System.out.println("5. Back to Main Menu");
+            System.out.print("Enter your choice: ");
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid! enter a number between 1 to 5.");
+                scanner.next(); // invalid input
+                continue;
+            }
+
+            assignmentChoice = scanner.nextInt();
+            scanner.nextLine(); // newline
+
+            switch (assignmentChoice) {
+                case 1:
+                    System.out.print("Enter  Course name: ");
+                    String title = scanner.nextLine().trim();
+                    System.out.print("Enter description: ");
+                    String description = scanner.nextLine().trim();
+                    System.out.print("Enter deadline (yyyy-mm-dd): ");
+                    String deadline = scanner.nextLine().trim();
+                    System.out.print("Enter priority (High, Medium, Low): ");
+                    String priority = scanner.nextLine().trim();
+                    assignmentService.addAssignment(title, description, deadline, priority);
+                    break;
+
+                case 2:
+                    assignmentService.displayAssignments();
+                    break;
+
+                case 3:
+                    System.out.print("Enter name of the assignment to update: ");
+                    String oldTitle = scanner.nextLine().trim();
+                    System.out.print("Enter new name: ");
+                    String newTitle = scanner.nextLine().trim();
+                    System.out.print("Enter new description: ");
+                    String newDescription = scanner.nextLine().trim();
+                    System.out.print("Enter new deadline: ");
+                    String newDeadline = scanner.nextLine().trim();
+                    System.out.print("Enter new priority: ");
+                    String newPriority = scanner.nextLine().trim();
+                    assignmentService.updateAssignment(oldTitle, newTitle, newDescription, newDeadline, newPriority);
+                    break;
+
+                case 4:
+                    System.out.print("Enter name of the assignment to delete: ");
+                    String deleteTitle = scanner.nextLine().trim();
+                    assignmentService.deleteAssignment(deleteTitle);
+                    break;
+
+                case 5:
+                    System.out.println("Returning to Main Menu...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (assignmentChoice != 5);
+    }
+}
