d4a550422ec1e2fba48faeb1fd448d2e1edc9594
nbv3 <nbv3@student.le.ac.uk>
2025-01-03 15:24:22 +0000

    Update ClassDetails.java
    Sprint-1, As a developer, I want to create a serialized structure to store class details such as subject, time, and instructor.

--- src/timetable/timetable.classmanagement/ClassDetails.java
+++ src/timetable/timetable.classmanagement/ClassDetails.java
@@ -1,1 +1,51 @@
+package timetable.classmanagement;
+import java.io.Serializable;
+
+public class ClassDetails implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String subject;
+    private String time;
+    private String instructor;
+
+    public ClassDetails(String subject, String time, String instructor) {
+        this.subject = subject;
+        this.time = time;
+        this.instructor = instructor;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassDetails{" +
+                "subject='" + subject + '\'' +
+                ", time='" + time + '\'' +
+                ", instructor='" + instructor + '\'' +
+                '}';
+    }
+}
+
 
