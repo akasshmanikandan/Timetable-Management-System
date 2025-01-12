6f7dce42f72c463e3dffc158818b67ca84a07fb4
nbv3 <nbv3@student.le.ac.uk>
2025-01-12 13:47:43 +0000

    Update Timetable.java

--- src/timetable/Timetable.java
+++ src/timetable/Timetable.java
@@ -4,24 +4,28 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Timetable implements Serializable {
-    private String day;
+   
+	private static final long serialVersionUID = 1L;
+	private String day;
     private String time;
     private String subject;
     private String teacher;
-    private String recurrence; // daily, weekly, or monthly
-
+    private String recurrence; 
+    private int reminderOffset;// daily, weekly, or monthly
+   
     //constructor with default recurrence
-    public Timetable(String day, String time, String subject, String teacher) {
-        this(day, time, subject, teacher, "None"); //default recurrence is "None"
+    public Timetable(String day, String time, String subject, String teacher,int reminderOffset) {
+        this(day, time, subject, teacher, "None",reminderOffset); //default recurrence is "None"
     }
 
    
-    public Timetable(String day, String time, String subject, String teacher, String recurrence) {
+    public Timetable(String day, String time, String subject, String teacher, String recurrenceint, int reminderOffset) {
         this.day = day;
-        this.time = time;
+        this.time = time.trim();
         this.subject = subject;
         this.teacher = teacher;
         this.recurrence = recurrence != null ? recurrence : "None";
+        this.reminderOffset = reminderOffset;
     }
 
     //getters and setters
@@ -64,6 +68,13 @@ public class Timetable implements Serializable {
     public void setRecurrence(String recurrence) {
         this.recurrence = recurrence != null ? recurrence : "None";
     }
+    public int getReminderOffset() {
+        return reminderOffset;
+    }
+
+    public void setReminderOffset(int reminderOffset) {
+        this.reminderOffset = reminderOffset;
+    }
 
     @Override
     public String toString() {
@@ -73,6 +84,7 @@ public class Timetable implements Serializable {
                 ", subject='" + subject + '\'' +
                 ", teacher='" + teacher + '\'' +
                 ", recurrence='" + recurrence + '\'' +
+                ", reminderOffset=" + reminderOffset + " minutes before event" +
                 '}';
     }
 
@@ -81,15 +93,17 @@ public class Timetable implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Timetable timetable = (Timetable) o;
-        return Objects.equals(day, timetable.day) &&
+        return reminderOffset == timetable.reminderOffset &&
+               Objects.equals(day, timetable.day) &&
                Objects.equals(time, timetable.time) &&
                Objects.equals(subject, timetable.subject) &&
                Objects.equals(teacher, timetable.teacher) &&
                Objects.equals(recurrence, timetable.recurrence);
+        
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(day, time, subject, teacher, recurrence);
+        return Objects.hash(day, time, subject, teacher, recurrence, reminderOffset);
     }
 }
